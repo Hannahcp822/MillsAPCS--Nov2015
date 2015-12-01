362b3062e95dc3d0dd9f474ebd88a5cadd5223c4
Hannahcp822 <hannahpang@Hannahs-Laptop.local>
2015-11-30 20:27:20 -0800

    Farcical

---
+++ SampleFracCalc/SampleFracCalc.java
@@ -0,0 +1,35 @@
+package SampleFracCalc;
+
+
+/**
+ * Write a description of class SampleFracCalc here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SampleFracCalc
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class SampleFracCalc
+     */
+    public SampleFracCalc()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
